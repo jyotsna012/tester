2a3f7b07035db298b51c57b2b1a2b43645c4cc25
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-02-09 22:01:36 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <iostream>
 
 using namespace std;
-char* hello();
+char hello();
 
 int main(){
   
@@ -10,7 +10,7 @@ int main(){
    
        char c1 ='0';
        char c2 ='4';
-       char* c3[3] = {c1,c2,0}; // 0 at the end to terminate the string
+       char c3[3] = {c1,c2,0}; // 0 at the end to terminate the string
        cout<< c3 << endl;
        return 0;
    
