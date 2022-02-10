6a0fcc250738cd4ae0fab2cbf2983188e7fa9982
jyotsna012 <88685258+jyotsna012@users.noreply.github.com>
2022-02-09 18:21:01 -0800

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,1 +1,20 @@
+#include <cstring>
+#include <iostream>
 
+using namespace std;
+char* test();
+
+int main(){
+  
+  cout << test() << endl;
+  
+}
+
+char* test(){
+
+  cout << "enter a num" << endl;
+  char array[80];
+  cin >> array;
+  return array;
+
+}
